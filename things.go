335e00eafb92f981f23b942548d71f1fbd801c11
Luna Stadler <lu@papill0n.org>
2024-08-11 23:20:13 +0200

    Add a favicon and viewport setting
    Maybe temporary, maybe not.  We shall see.

--- things.go
+++ things.go
@@ -76,9 +76,11 @@ func pageWithContent(w http.ResponseWriter, req *http.Request, input string, con
 <html>
 <head>
 	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
 	<title>things</title>
 
 	<link rel="stylesheet" href="/static/things.css" />
+	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%%22http://www.w3.org/2000/svg%%22 viewBox=%%220 0 100 100%%22><text y=%%22.9em%%22 font-size=%%2290%%22>🐦‍⬛</text></svg>" />
 </head>
 
 <body>
